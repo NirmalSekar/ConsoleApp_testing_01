791a16b78a2ede502365f2f1c87e6e5264591a69
Nirmal Kumar Sekar <nxsekar1@shockers.wichita.edu>
2022-10-11 08:15:56 -0500

    printing changes

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,21 +10,7 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Succesfully pushed to the Github");
-           
-
-            string name, surname;
-
-            Console.WriteLine("Enter your name : ");
-            name = Console.ReadLine();
-
-            Console.WriteLine("Enter your last name: ");
-            surname = Console.ReadLine();
-
-            Console.WriteLine("Your name is {0} , your surname is {1}", name, surname);
-
-
-            Console.WriteLine($"this type of output can also be displayed{name}, {surname}");
+            Console.WriteLine("Hi my name is nirmal");
             Console.ReadKey();
         }
     }
